23e667f9f63a4e0928914d6969d3ccf5368c76c9
Aaryan-R-S <aaryan20004@iiitd.ac.in>
2021-05-31 10:45:15 +0530

    Added more Tuts

---
+++ Tutorials/t95extendedEuclidAlgo.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Diophantine Equations--
+// Polynomial equations for which integral solution exists e.g. ax+by=c where
+// GCD of a, b has properety that -- gcd(a,b) divides c then
+// ax+by=gcd(a,b) for eqn to be Diophantine
+
+struct triplet{
+    int x, y, gcd;
+};
+
+triplet extendedEuclidAlgo(int a, int b){
+    if(b==0){
+        triplet ans;
+        ans.x = 1;
+        ans.y = 0;  // any value u can give
+        ans.gcd = a;
+        return ans;
+    }
+    triplet smallAns = extendedEuclidAlgo(b, a%b);
+    triplet ans;
+    ans.gcd = smallAns.gcd;
+    ans.x = smallAns.y;
+    ans.y = smallAns.x-(a/b)*smallAns.y;
+    return ans;
+}
+
+// Multiplicative Modulo Inverse
+// Find B such that (A*B)%m = 1  (Note A and m must be co-prime for B to exist)
+// Above eqn can be written as Ax+qm = 1 where x = B and q = -(A*B)/m 
+int multModInverse(int A, int m){
+    triplet ans = extendedEuclidAlgo(A, m);
+    // a%m = (a+m)%m so use this in return to avoid -ve x if any;
+    return ((ans.x<0) ? (ans.x+m) : (ans.x));  
+}
+
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+
+    // triplet ans = extendedEuclidAlgo(a,b);
+    // cout<<ans.gcd<<" "<<ans.x<<" "<<ans.y<<endl;
+
+    cout<<multModInverse(a,b)<<endl;
+
+    return 0;
+}
